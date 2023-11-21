c9b54f4973661c77754868dedec17150178ab96b
sergeavila7 <serge_avila7@yahoo.com>
2023-11-21 17:41:01 -0600

    fix: workflow

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -4,7 +4,7 @@ import os
 def main():
     lenguaje_favorito = os.getenv("LANGUAGE")
     nombre = os.getenv("USERNAME")
-    print(f"¡Hola, mundo desde {lenguaje} soy {nombre}!")
+    print(f"¡Hola, mundo desde {lenguaje_favorito} soy {nombre}!")
 
 
 if __name__ == "__main__":
